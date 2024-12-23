59101a8a211bcad7ba964ec330e3ddca492e5581
gki-webik <gki-webik@yandex.ru>
2024-12-23 22:11:18 +0600

    replace amount

--- src/components/Type/data.js
+++ src/components/Type/data.js
@@ -13,14 +13,14 @@ export const data = {
             {
               id: "bracelet-1-type-1",
               value: "Узкий (4 мм)",
-              price: "3190",
+              price: "3180",
               active: true,
               photo: "https://static.tildacdn.com/tild3366-3235-4638-b434-316366393832/tupe-1.jpg",
             },
             {
               id: "bracelet-1-type-2",
               value: "Широкий (6 мм)",
-              price: "3190",
+              price: "3180",
               photo: "https://static.tildacdn.com/tild3530-3730-4432-b231-393131656534/type-2.jpg",
             },
           ],
